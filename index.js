6a64fc0eaf7db5f0e3f7fa98ee7f7cce0f48293f
m7real <masayeedreal@gmail.com>
2022-11-08 12:10:06 +0600

    mongodb basic setup with env variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wo3xvdt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    const serviceCollection = client.db("foodFly").collection("services");
+    const reviewCollection = client.db("foodFly").collection("reviews");
+  } finally {
+    // prettier-ignore
+}
+}
+
+run().catch((err) => console.error(err));
 // test api
 app.get("/", (req, res) => {
   res.send("Food Fly Server Running");
