85cb0e922041c219040321907fe1252c1f82675e
m7real <masayeedreal@gmail.com>
2022-11-08 11:54:55 +0600

    basic test api and listen added

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+// test api
+app.get("/", (req, res) => {
+  res.send("Food Fly Server Running");
+});
+
+app.listen(port, () => {
+  console.log("Food Fly Server Running On Port", port);
+});
