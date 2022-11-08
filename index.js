e1a32d025ab75d5f152656c8477bab8f98812c82
m7real <masayeedreal@gmail.com>
2022-11-08 11:50:19 +0600

    requirments added

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
