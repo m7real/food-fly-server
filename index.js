f5f850429e279c83154cc43c551ea628b0f7a851
m7real <masayeedreal@gmail.com>
2022-11-09 16:38:10 +0600

    api added for getting single service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -24,11 +24,13 @@ async function run() {
     app.get("/services", async (req, res) => {
       const count = parseInt(req.query.count);
       const query = {};
-      const cursor = serviceCollection.find(query).sort({ _id: -1 });
       if (count) {
+        // latest added service will show on homepage
+        const cursor = serviceCollection.find(query).sort({ _id: -1 });
         const services = await cursor.limit(count).toArray();
         res.send(services);
       } else {
+        const cursor = serviceCollection.find(query);
         const services = await cursor.toArray();
         res.send(services);
       }
@@ -39,6 +41,15 @@ async function run() {
       const service = req.body;
       const result = await serviceCollection.insertOne(service);
       res.send(result);
+    });
+
+    // get single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
   } finally {
     // prettier-ignore
